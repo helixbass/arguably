bd13abcd3c751e6d1f88eb4a000e43b7d2b1537e
Julian Rosse <julian@helixbass.net>
2020-07-27 23:25:01 -0400

    drug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,127 @@
 import React, {FC} from 'react'
 import {flowMax, addDisplayName} from 'ad-hok'
+import {addLayoutEffectOnMount} from 'ad-hok-utils'
+import gsap from 'gsap'
 
 import {makeStyles} from 'utils/style'
 import colors from 'utils/colors'
+import {addRefsContext, addRefsContextProvider} from 'utils/refsContext'
+import {addRefs} from 'utils/refs'
+import {gothicFontStyles} from 'utils/typography'
+import addTypekit from 'utils/addTypekit'
 
-const App: FC = flowMax(addDisplayName('App'), () => (
-  <div css={styles.container}>Hello world!</div>
-))
+const BLUR_FILTER_ID = 'blur-filter'
+const DRUG_MASK_ID = 'drug-mask'
+
+const FilterDef: FC = flowMax(
+  addDisplayName('FilterDef'),
+  addRefsContext,
+  ({setRef}) => (
+    <svg height="100vh" width="100vh">
+      <defs>
+        <filter id={BLUR_FILTER_ID}>
+          <feGaussianBlur
+            ref={setRef('blur')}
+            stdDeviation={10}
+            result="BLUR"
+          />
+          <feFlood
+            ref={setRef('flood')}
+            floodColor="#33ee22"
+            floodOpacity={0.3}
+            in="SourceGraphic"
+            result="FLOOD"
+          />
+          <feMerge>
+            <feMergeNode in="BLUR" />
+            <feMergeNode in="FLOOD" />
+          </feMerge>
+        </filter>
+        <mask id={DRUG_MASK_ID}>
+          <rect x={0} y={0} fill="white" width="100%" height="100%" />
+          <text
+            ref={setRef('drug')}
+            fill="black"
+            x="50%"
+            y="50%"
+            textAnchor="middle"
+            css={styles.drug}
+            dy="50"
+          >
+            drug
+          </text>
+        </mask>
+      </defs>
+      <image
+        ref={setRef('image')}
+        filter={`url(#${BLUR_FILTER_ID})`}
+        href="matthew-henry-nvFpb_MMRj8-unsplash.jpg"
+        height="100vh"
+      />
+      <rect
+        ref={setRef('black')}
+        x={0}
+        y={0}
+        fill={colors.black}
+        width="100%"
+        height="100%"
+        mask={`url(#${DRUG_MASK_ID})`}
+      />
+    </svg>
+  ),
+)
+
+const App: FC = flowMax(
+  addDisplayName('App'),
+  addTypekit('tmz1qbf'),
+  addRefs,
+  addRefsContextProvider,
+  addLayoutEffectOnMount(({refs}) => () => {
+    const {
+      blur,
+      image,
+      // drug,
+      black,
+    } = refs
+
+    gsap
+      .timeline({paused: true})
+      .set(black, {
+        opacity: 0,
+      })
+      .to(
+        blur,
+        {
+          attr: {stdDeviation: 0},
+          duration: 0.2,
+          ease: 'linear',
+        },
+        0.6,
+      )
+      .to(
+        black,
+        {
+          opacity: 1,
+          duration: 0.01,
+        },
+        '+=0.2',
+      )
+      .to(image, {
+        x: 50,
+        y: 21,
+        duration: 20,
+        repeat: -1,
+        yoyo: true,
+      })
+      .play()
+  }),
+  () => (
+    <div css={styles.container}>
+      <FilterDef />
+      <span>Hello world!</span>
+    </div>
+  ),
+)
 
 export default App
 
@@ -16,8 +131,13 @@ const styles = makeStyles({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: colors.black,
     color: colors.white,
     minHeight: '100vh',
+  },
+  drug: {
+    fontSize: 235,
+    ...gothicFontStyles,
+    textTransform: 'uppercase',
+    letterSpacing: 1.5,
   },
 })
---
+++ src/utils/typography.ts
@@ -0,0 +1,6 @@
+const gothicFontFamily = 'triplex-cond-sans, sans-serif'
+
+export const gothicFontStyles = {
+  fontFamily: gothicFontFamily,
+  fontWeight: 800,
+}
